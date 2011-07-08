6033ed28d2a57f3735bf8b83e640dc8eba3039c8
Micah Wittman <mwittman@gmail.com>
2011-07-08 02:29:06 -0600

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# GooglePlusPlus Hide Images
+
+`googleplusplus_hide_images` is a user script for Google Plus (aka Google+ aka G+) that adds a button under each Google Plus post image to hide it (toggle show / hide). Hides the image on original post *and all shared posts*. Tired of an animated GIF that keep getting shared over and over in different posts? - hide it once an forget about it.
+
+So far, this user script was tested in Google Chrome 12 and Firefox 5 (Greasemonkey required) (on OS X).
+
+Feedback is welcome.
---
+++ googleplusplus_hide_images.user.js
@@ -0,0 +1,409 @@
+// ==UserScript==
+// @name           googleplusplus_hide_images
+// @author         Micah Wittman
+// @namespace      http://wittman.org/projects/googleplusplus_hide_images
+// @include        *plus.google.com*
+// @description    Adds button under each Google Plus post image to hide it (toggle show / hide). Hides the image on original post and all shared posts. Tired of an animated GIF that keep getting shared over and over in different posts? - hide it once an forget about it.
+// @version        0.1.0
+// ==/UserScript==
+
+function hideImages(){
+	
+	/****** Utility functions ******/
+	function log(txt) {
+		if(logging) {
+			console.log(txt);
+		}
+	}
+
+	/*
+	* Add integers, wrapping at 2^32. This uses 16-bit operations internally
+	* to work around bugs in some JS interpreters.
+	*/
+	function safe_add(x, y) {
+		var lsw = (x & 0xFFFF) + (y & 0xFFFF),
+		msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+		return (msw << 16) | (lsw & 0xFFFF);
+	}
+
+	/*
+	* Bitwise rotate a 32-bit number to the left.
+	*/
+	function bit_rol(num, cnt) {
+		return (num << cnt) | (num >>> (32 - cnt));
+	}
+
+	/*
+	* These functions implement the four basic operations the algorithm uses.
+	*/
+	function md5_cmn(q, a, b, x, s, t) {
+		return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s), b);
+	}
+	function md5_ff(a, b, c, d, x, s, t) {
+		return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t);
+	}
+	function md5_gg(a, b, c, d, x, s, t) {
+		return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t);
+	}
+	function md5_hh(a, b, c, d, x, s, t) {
+		return md5_cmn(b ^ c ^ d, a, b, x, s, t);
+	}
+	function md5_ii(a, b, c, d, x, s, t) {
+		return md5_cmn(c ^ (b | (~d)), a, b, x, s, t);
+	}
+
+	/*
+	* Calculate the MD5 of an array of little-endian words, and a bit length.
+	*/
+	function binl_md5(x, len) {
+		/* append padding */
+		x[len >> 5] |= 0x80 << ((len) % 32);
+		x[(((len + 64) >>> 9) << 4) + 14] = len;
+
+		var i, olda, oldb, oldc, oldd,
+		a =  1732584193,
+		b = -271733879,
+		c = -1732584194,
+		d =  271733878;
+
+		for (i = 0; i < x.length; i += 16) {
+			olda = a;
+			oldb = b;
+			oldc = c;
+			oldd = d;
+
+			a = md5_ff(a, b, c, d, x[i],       7, -680876936);
+			d = md5_ff(d, a, b, c, x[i +  1], 12, -389564586);
+			c = md5_ff(c, d, a, b, x[i +  2], 17,  606105819);
+			b = md5_ff(b, c, d, a, x[i +  3], 22, -1044525330);
+			a = md5_ff(a, b, c, d, x[i +  4],  7, -176418897);
+			d = md5_ff(d, a, b, c, x[i +  5], 12,  1200080426);
+			c = md5_ff(c, d, a, b, x[i +  6], 17, -1473231341);
+			b = md5_ff(b, c, d, a, x[i +  7], 22, -45705983);
+			a = md5_ff(a, b, c, d, x[i +  8],  7,  1770035416);
+			d = md5_ff(d, a, b, c, x[i +  9], 12, -1958414417);
+			c = md5_ff(c, d, a, b, x[i + 10], 17, -42063);
+			b = md5_ff(b, c, d, a, x[i + 11], 22, -1990404162);
+			a = md5_ff(a, b, c, d, x[i + 12],  7,  1804603682);
+			d = md5_ff(d, a, b, c, x[i + 13], 12, -40341101);
+			c = md5_ff(c, d, a, b, x[i + 14], 17, -1502002290);
+			b = md5_ff(b, c, d, a, x[i + 15], 22,  1236535329);
+
+			a = md5_gg(a, b, c, d, x[i +  1],  5, -165796510);
+			d = md5_gg(d, a, b, c, x[i +  6],  9, -1069501632);
+			c = md5_gg(c, d, a, b, x[i + 11], 14,  643717713);
+			b = md5_gg(b, c, d, a, x[i],      20, -373897302);
+			a = md5_gg(a, b, c, d, x[i +  5],  5, -701558691);
+			d = md5_gg(d, a, b, c, x[i + 10],  9,  38016083);
+			c = md5_gg(c, d, a, b, x[i + 15], 14, -660478335);
+			b = md5_gg(b, c, d, a, x[i +  4], 20, -405537848);
+			a = md5_gg(a, b, c, d, x[i +  9],  5,  568446438);
+			d = md5_gg(d, a, b, c, x[i + 14],  9, -1019803690);
+			c = md5_gg(c, d, a, b, x[i +  3], 14, -187363961);
+			b = md5_gg(b, c, d, a, x[i +  8], 20,  1163531501);
+			a = md5_gg(a, b, c, d, x[i + 13],  5, -1444681467);
+			d = md5_gg(d, a, b, c, x[i +  2],  9, -51403784);
+			c = md5_gg(c, d, a, b, x[i +  7], 14,  1735328473);
+			b = md5_gg(b, c, d, a, x[i + 12], 20, -1926607734);
+
+			a = md5_hh(a, b, c, d, x[i +  5],  4, -378558);
+			d = md5_hh(d, a, b, c, x[i +  8], 11, -2022574463);
+			c = md5_hh(c, d, a, b, x[i + 11], 16,  1839030562);
+			b = md5_hh(b, c, d, a, x[i + 14], 23, -35309556);
+			a = md5_hh(a, b, c, d, x[i +  1],  4, -1530992060);
+			d = md5_hh(d, a, b, c, x[i +  4], 11,  1272893353);
+			c = md5_hh(c, d, a, b, x[i +  7], 16, -155497632);
+			b = md5_hh(b, c, d, a, x[i + 10], 23, -1094730640);
+			a = md5_hh(a, b, c, d, x[i + 13],  4,  681279174);
+			d = md5_hh(d, a, b, c, x[i],      11, -358537222);
+			c = md5_hh(c, d, a, b, x[i +  3], 16, -722521979);
+			b = md5_hh(b, c, d, a, x[i +  6], 23,  76029189);
+			a = md5_hh(a, b, c, d, x[i +  9],  4, -640364487);
+			d = md5_hh(d, a, b, c, x[i + 12], 11, -421815835);
+			c = md5_hh(c, d, a, b, x[i + 15], 16,  530742520);
+			b = md5_hh(b, c, d, a, x[i +  2], 23, -995338651);
+
+			a = md5_ii(a, b, c, d, x[i],       6, -198630844);
+			d = md5_ii(d, a, b, c, x[i +  7], 10,  1126891415);
+			c = md5_ii(c, d, a, b, x[i + 14], 15, -1416354905);
+			b = md5_ii(b, c, d, a, x[i +  5], 21, -57434055);
+			a = md5_ii(a, b, c, d, x[i + 12],  6,  1700485571);
+			d = md5_ii(d, a, b, c, x[i +  3], 10, -1894986606);
+			c = md5_ii(c, d, a, b, x[i + 10], 15, -1051523);
+			b = md5_ii(b, c, d, a, x[i +  1], 21, -2054922799);
+			a = md5_ii(a, b, c, d, x[i +  8],  6,  1873313359);
+			d = md5_ii(d, a, b, c, x[i + 15], 10, -30611744);
+			c = md5_ii(c, d, a, b, x[i +  6], 15, -1560198380);
+			b = md5_ii(b, c, d, a, x[i + 13], 21,  1309151649);
+			a = md5_ii(a, b, c, d, x[i +  4],  6, -145523070);
+			d = md5_ii(d, a, b, c, x[i + 11], 10, -1120210379);
+			c = md5_ii(c, d, a, b, x[i +  2], 15,  718787259);
+			b = md5_ii(b, c, d, a, x[i +  9], 21, -343485551);
+
+			a = safe_add(a, olda);
+			b = safe_add(b, oldb);
+			c = safe_add(c, oldc);
+			d = safe_add(d, oldd);
+		}
+		return [a, b, c, d];
+	}
+
+	/*
+	* Convert an array of little-endian words to a string
+	*/
+	function binl2rstr(input) {
+		var i,
+		output = '';
+		for (i = 0; i < input.length * 32; i += 8) {
+			output += String.fromCharCode((input[i >> 5] >>> (i % 32)) & 0xFF);
+		}
+		return output;
+	}
+
+	/*
+	* Convert a raw string to an array of little-endian words
+	* Characters >255 have their high-byte silently ignored.
+	*/
+	function rstr2binl(input) {
+		var i,
+		output = [];
+		output[(input.length >> 2) - 1] = undefined;
+		for (i = 0; i < output.length; i += 1) {
+			output[i] = 0;
+		}
+		for (i = 0; i < input.length * 8; i += 8) {
+			output[i >> 5] |= (input.charCodeAt(i / 8) & 0xFF) << (i % 32);
+		}
+		return output;
+	}
+
+	/*
+	* Calculate the MD5 of a raw string
+	*/
+	function rstr_md5(s) {
+		return binl2rstr(binl_md5(rstr2binl(s), s.length * 8));
+	}
+
+	/*
+	* Calculate the HMAC-MD5, of a key and some data (raw strings)
+	*/
+	function rstr_hmac_md5(key, data) {
+		var i,
+		bkey = rstr2binl(key),
+		ipad = [],
+		opad = [],
+		hash;
+		ipad[15] = opad[15] = undefined;                        
+		if (bkey.length > 16) {
+			bkey = binl_md5(bkey, key.length * 8);
+		}
+		for (i = 0; i < 16; i += 1) {
+			ipad[i] = bkey[i] ^ 0x36363636;
+			opad[i] = bkey[i] ^ 0x5C5C5C5C;
+		}
+		hash = binl_md5(ipad.concat(rstr2binl(data)), 512 + data.length * 8);
+		return binl2rstr(binl_md5(opad.concat(hash), 512 + 128));
+	}
+
+	/*
+	* Convert a raw string to a hex string
+	*/
+	function rstr2hex(input) {
+		var hex_tab = '0123456789abcdef',
+		output = '',
+		x,
+		i;
+		for (i = 0; i < input.length; i += 1) {
+			x = input.charCodeAt(i);
+			output += hex_tab.charAt((x >>> 4) & 0x0F) +
+			hex_tab.charAt(x & 0x0F);
+		}
+		return output;
+	}
+
+	/*
+	* Encode a string as utf-8
+	*/
+	function str2rstr_utf8(input) {
+		return unescape(encodeURIComponent(input));
+	}
+
+	/*
+	* Take string arguments and return either raw or hex encoded strings
+	*/
+	function raw_md5(s) {
+		return rstr_md5(str2rstr_utf8(s));
+	}
+	function hex_md5(s) {
+		return rstr2hex(raw_md5(s));
+	}
+	function raw_hmac_md5(k, d) {
+		return rstr_hmac_md5(str2rstr_utf8(k), str2rstr_utf8(d));
+	}
+	function hex_hmac_md5(k, d) {
+		return rstr2hex(raw_hmac_md5(k, d));
+	}
+	/*
+	* jQuery MD5 Plugin 1.2.1
+	* https://github.com/blueimp/jQuery-MD5
+	*
+	* Copyright 2010, Sebastian Tschan
+	* https://blueimp.net
+	*
+	* Licensed under the MIT license:
+	* http://creativecommons.org/licenses/MIT/
+	* 
+	* Based on
+	* A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
+	* Digest Algorithm, as defined in RFC 1321.
+	* Version 2.2 Copyright (C) Paul Johnston 1999 - 2009
+	* Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
+	* Distributed under the BSD License
+	* See http://pajhome.org.uk/crypt/md5 for more info.
+	*/
+	function md5(string, key, raw) {
+		if (!key) {
+			if (!raw) {
+				return hex_md5(string);
+			} else {
+				return raw_md5(string);
+			}
+		}
+		if (!raw) {
+			return hex_hmac_md5(key, string);
+		} else {
+			return raw_hmac_md5(key, string);
+		}
+	}
+	function setItem(key, value) {
+		try{
+			log("Inside setItem: " + key + ":" + value);
+			window.localStorage.removeItem(key);
+			window.localStorage.setItem(key, value);
+		}catch(e){
+			log("Error inside setItem");
+			log(e);
+		}
+		log("Return from setItem" + key + ":" +  value);
+	}
+
+	function getItem(key){
+		var v;
+		log('Get Item: ' + key);
+		try{
+			v = window.localStorage.getItem(key);
+		}catch(e){
+			log("Error inside getItem() for key: " + key);
+			log(e);
+			v = null;
+		}
+		log("Returning value: " + v);
+		return v;
+	}
+	function removeItem(key) {
+		try{
+			log("Inside removetItem: " + key);
+			window.localStorage.removeItem(key);
+		}catch(e){
+			log("Error inside removeItem");
+			log(e);
+		}
+		log("Return from removeItem" + key);
+	}
+	function clearStorage(){
+		log('about to clear local storage');
+		window.localStorage.clear();
+		log('cleared');
+	}
+	function GM_removeItem(name){
+		removeItem(name);
+	}
+	function GM_setValue(name, value){
+		setItem(name, value);
+	}
+
+	function GM_getValue(name, oDefault){
+		var v = getItem(name);
+		if(v == null){
+			return oDefault;
+		}else{
+			return v;
+		}
+	}
+
+	/****** Helper functions ******/
+	function isThumbnail(img){
+		return (img.height() == 46);
+	}
+
+	/****** Constants ******/
+	var logging = false;
+
+	/****** Before Loop Variables ******/
+	var i = 0;
+	var img_divs = $('#contentPane .a-b-f-S-oa div[data-content-url]');
+	
+	/****** Loop ******/
+	function main_loop(){
+		
+		img_divs = $('#contentPane .a-b-f-S-oa div[data-content-url]');
+		img_count = img_divs.length;
+			
+		img_divs.each(function(){
+			var t = $(this);
+			var img_url = t.attr('data-content-url');
+			var url_hash;
+			if( !t.hasClass('gpp__hide_images_tagged') && !isThumbnail(t) ){
+				//Process new images
+				url_hash = md5(img_url);
+				t.addClass('gpp__hide_images_tagged');
+				t.after('<div id="gpp__hide_images_button_' + i + '" style="height: 8px;width: 91%;" class="gpp__hide_images a-f-i-WXPuNd a-b-f-i-W-xb"><span role="button" class="d-h a-b-f-i-gc-cf-Xb-h" tabindex="0"><span style="margin-top:-5px" class="n-Wa-q n-Wa-q-z" title="Hidden image"></span><span style="font-size:9px;margin:0 0 0 4px;position:absolute"><a>SHOW / HIDE</a></span></span></div>');
+				var img = t;
+				var button = img.parent().find('#gpp__hide_images_button_' + i + ':first');
+				button.click(function(){
+					if( img.is(':visible') ){
+						//Hide
+						GM_setValue('gpp__hidden_img_url_' + url_hash, true);
+					}else{
+						//Show
+						GM_removeItem('gpp__hidden_img_url_' + url_hash);
+					}
+					//Toggle show/hide
+					img.toggle();
+				});
+				i++;
+			}else{
+				//Process existing images
+				if( t.is(':visible') ){
+					url_hash = md5(img_url);
+					var hidden_img_url = GM_getValue('gpp__hidden_img_url_' + url_hash, false);
+					if(hidden_img_url){
+						t.hide();
+					}
+				}
+			}
+			
+		});
+		
+	}
+	
+	/****** Start Loop ******/
+	main_loop();
+	setInterval(main_loop, 2000);	
+
+}
+
+/****** Load jQuery then callback upon load function ******/
+function addJQuery(callback){
+	var script = document.createElement("script");
+	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js");
+	script.addEventListener('load', function() {
+		var script = document.createElement("script");
+		script.textContent = "(" + callback.toString() + ")();";
+		document.body.appendChild(script);
+	}, false);
+	document.body.appendChild(script);
+}
+
+/****** Call Load jQuery + callback function ******/
+addJQuery(hideImages);
